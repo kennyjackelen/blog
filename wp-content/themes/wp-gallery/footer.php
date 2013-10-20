96ece7aa600aaa4180dd7a236d0bce08141ddff5
Kenny Jackelen <kenny@kennyjackelen.com>
2013-10-19 19:01:02 -0500

    upgrade to jQuery 1.10.2

--- wp-content/themes/wp-gallery/footer.php
+++ wp-content/themes/wp-gallery/footer.php
@@ -12,8 +12,8 @@
   </div><!-- #wrapper -->
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.10.2.min.js"><\/script>')</script>
 
   <!-- start scripts -->
   <script src="<?php bloginfo('template_url'); ?>/js/libs/min/bootstrap.min.js"></script>
