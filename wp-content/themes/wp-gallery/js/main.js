889022068b0a11f431f78e79b2a51af64147c299
Kenny Jackelen <kenny@kennyjackelen.com>
2013-10-21 19:09:01 -0500

    implement swipe actions in gallery

--- wp-content/themes/wp-gallery/js/main.js
+++ wp-content/themes/wp-gallery/js/main.js
@@ -4,7 +4,33 @@ function onPageLoad()
 	$('.gallery').magnificPopup( {
 		delegate: 'a',
 		type: 'image',
-		gallery: { enabled: true }
+		gallery: {
+			enabled: true,
+			navigateByImgClick: false,
+		},
+		callbacks: {
+			imageLoadComplete: setupSwipe
+		}
+	});
+}
+
+function setupSwipe(){
+	$('.mfp-img').mousedown( function(event) {
+		event.preventDefault();
+	});
+	$('.mfp-img').swipe({
+		swipeLeft: function(event, direction, distance, duration, fingerCount) {
+			$.magnificPopup.instance.next();
+		},
+		swipeRight: function(event, direction, distance, duration, fingerCount) {
+			$.magnificPopup.instance.prev();
+		},
+		tap: function(event, direction, distance, duration, fingerCount) {
+			$.magnificPopup.instance.next();
+		},
+		doubleTap: function(event, direction, distance, duration, fingerCount) {
+			window.open($.magnificPopup.instance.currItem.src,'_blank');
+		}
 	});
 }
 
